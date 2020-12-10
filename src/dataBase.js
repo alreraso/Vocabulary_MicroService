76377c81a81000217fc7a39ec00cbce3b42e13cf
mafecarbo <30605015+mafecarbo@users.noreply.github.com>
2020-12-10 18:49:46 -0500

    commit

--- src/dataBase.js
+++ src/dataBase.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://3.238.233.96:27019/vocabularyDB',{
+mongoose.connect('mongodb://3.238.233.96:30000/vocabularyDB',{
     userNewUrlParser: true,
     userCreateIndex: true
 });

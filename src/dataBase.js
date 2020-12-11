ddccaac45811acf34a3582d03bc18521c37cf0a2
mafecarbo <30605015+mafecarbo@users.noreply.github.com>
2020-12-11 10:35:14 -0500

    commit

--- src/dataBase.js
+++ src/dataBase.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://3.239.124.145:30000/vocabularyDB',{
+mongoose.connect('mongodb+srv://mafe:mafe123@vocabularydb.hfggs.mongodb.net/vocabularyDB?retryWrites=true&w=majority',{
     userNewUrlParser: true,
     userCreateIndex: true
 });
-
 
 const connection= mongoose.connection;
 
 connection.once('open',() => {
     console.log("DB is connected");
 });
+
 
 module.exports = mongoose;
